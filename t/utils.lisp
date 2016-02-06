da1a6da95f0718fad8811718ed606c15908b7152
eshamster <hamgoostar@gmail.com>
2016-02-06 16:40:26 +0900

    Add functions and macros for hook

--- t/utils.lisp
+++ t/utils.lisp
@@ -10,7 +10,7 @@
                 :is-list.ps+))
 (in-package :cl-ps-ecs-test.utils)
 
-(plan 1)
+(plan 2)
 
 (defstruct.ps+ test1 a b)
 (defstruct.ps+ (test2 (:include test1)) c)
@@ -30,5 +30,50 @@
   (prove-in-both (ok (includes-all-component-types
                       '()
                       (list (make-test1) (make-test2))))))
+
+(defstruct.ps+ test-st-hook1 a)
+(defstruct.ps+ test-st-hook2 b)
+
+(defvar.ps+ *test-var-hook* 0)
+
+(defun.ps+ test-fn-hook1 (object is-added)
+  (declare (ignore object))
+  (if is-added
+      (incf *test-var-hook*)
+      (decf *test-var-hook*)))
+
+(defun.ps+ test-fn-hook2 (object is-added)
+  (declare (ignore object))
+  (if is-added
+      (incf *test-var-hook* 10)
+      (decf *test-var-hook* 10)))
+
+(defun.ps+ init-test-hooks ()
+  (let ((hooks '()))
+    (push-to-ecs-hooks #'test-fn-hook1 hooks)
+    (push-to-ecs-hooks #'test-fn-hook2 hooks)))
+
+(subtest
+    "Test functions for hook"
+  (prove-in-both (is (let ((hooks (init-test-hooks)))
+                       (call-ecs-hooks (make-test-st-hook1) 'test-st-hook1 t hooks)
+                       *test-var-hook*)
+                     11))
+  (setf *test-var-hook* 0)
+  (prove-in-both (is (let ((hooks (init-test-hooks)))
+                       (call-ecs-hooks (make-test-st-hook1) 'test-st-hook1 nil hooks)
+                       *test-var-hook*)
+                     -11))
+  (setf *test-var-hook* 0)
+  (prove-in-both (is (let ((hooks (init-test-hooks)))
+                       (call-ecs-hooks (make-test-st-hook1) 'test-st-hook1 t hooks)
+                       (delete-from-ecs-hooks #'test-fn-hook2 hooks)
+                       (call-ecs-hooks (make-test-st-hook1) 'test-st-hook1 t hooks)
+                       *test-var-hook*)
+                     12))
+  (setf *test-var-hook* 0)
+
+  (prove-in-both (is-error (call-ecs-hooks (make-test-st-hook1) 'test-st-hook2 t '())
+                           'type-error)))
 
 (finalize)
