ca396088d5da303854124365a267a06aee8d6a02
eshamster <hamgoostar@gmail.com>
2016-08-27 16:56:46 +0000

    Export some elements

--- src/ecs.lisp
+++ src/ecs.lisp
@@ -26,8 +26,10 @@
            :delete-entity-tag
            
            :ecs-system
+           :target-entities
            :target-component-types
            :process
+           :process-all
            :add-entity-hook
            :delete-entity-hook
            
